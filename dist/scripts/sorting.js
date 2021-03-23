1ef7b2d17630395e07a7278aae424617d2204465
Divyansh Thakur <51845984+DivyanshThakur@users.noreply.github.com>
2021-03-23 15:15:51 +0530

    Bug fixes and ui improvement

---
+++ dist/scripts/sorting.js
@@ -0,0 +1,66 @@
+const barArray = [];
+const max_size = 1000;
+let size = 60;
+
+$(".btn-array").click(randomize);
+$("#arr_size").on("input", addBars);
+
+$("body").ready(function() {
+
+    const bars = $("#bars");
+
+    for(var i = 0; i<max_size; i++) {
+        const bar = document.createElement("div");
+        // const val = document.createElement("div");
+        // const bar = document.createElement("div");
+
+        $(bar).addClass("bar").css("height", "0px");
+        // $(val).addClass("val").text(1);
+        // $(barContainer).addClass("bar-container")
+        //                .append(bar)
+        //                .append(val);
+
+        barArray.push(bar);
+    }
+
+        addBars();   
+        setTimeout(() => randomize(), 500); 
+        // randomize();
+});
+
+
+function getRandom() {
+    return Math.floor(Math.random()*100) + 1;
+}
+
+function randomize() {
+    barArray.forEach(function(bar) {
+        const num = getRandom();
+        $(bar).css("height", num*4+ "px");
+    });
+}
+
+function addBars() {
+    size = $("#arr_size").val();
+    
+    $("#bar-size").text(size);
+
+    const bars = $("#bars");
+    bars.empty();
+
+    for(var i=0; i<size; i++) {
+        const bar = barArray[i];
+        bars.append(bar);
+    }
+}
+
+function updateSpeed() {
+    const speed = document.querySelector("#sort_speed").value;
+    const speedText = document.getElementById("speedText");
+    speedText.innerHTML = speed;
+}
+
+$('.toggle').click(function(e){
+    e.preventDefault(); // The flicker is a codepen thing
+    $(this).toggleClass('toggle-on');
+  });
